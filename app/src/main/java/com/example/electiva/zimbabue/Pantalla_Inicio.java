a1477ec9c8003222b42c573332058d1dae4faa4a
Ariel Montero Monestel <arielmmonestel@gmail.com>
2016-03-17 22:03:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/electiva/zimbabue/Pantalla_Inicio.java
@@ -0,0 +1,13 @@
+package com.example.electiva.zimbabue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla_Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio);
+    }
+}
