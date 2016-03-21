512a5702bf390e54387d19e948c9b525ae7c60a6
luisrra17 <luisrra17@gmail.com>
2016-03-21 12:59:41 -0600

    Odio Github Con mi Corazon

--- app/src/main/java/com/example/electiva/zimbabue/Pantalla_Inicio.java
+++ app/src/main/java/com/example/electiva/zimbabue/Pantalla_Inicio.java
@@ -14,6 +14,10 @@ public class Pantalla_Inicio extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_pantalla_inicio);
