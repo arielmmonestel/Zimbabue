aa0f496b7368e834d0be11a1c5a32d5c598de711
luisrra17 <luisrra17@gmail.com>
2016-03-21 14:57:31 -0600

    Odio Github Con mi Corazon

--- app/src/main/java/com/example/electiva/zimbabue/Cant_Jugadores.java
+++ app/src/main/java/com/example/electiva/zimbabue/Cant_Jugadores.java
@@ -2,7 +2,6 @@ package com.example.electiva.zimbabue;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -11,25 +10,21 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
-public class Pantalla_Inicio extends Activity {
-    private MediaPlayer fondo;
-
+public class Cant_Jugadores extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_pantalla_inicio);
-        fondo = MediaPlayer.create(this, R.raw.fondo);
-        final Button botonJugar= (Button) findViewById(R.id.buttonJugar);
-        final Button botonAyuda = (Button) findViewById(R.id.buttonAyuda);
-        fondo.setLooping(true);
-        fondo.start();
-        botonJugar.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_cant_jugador);
+        final Button Jugador2 = (Button) findViewById(R.id.Jugador2);
+        final Button Jugador3 = (Button) findViewById(R.id.Jugador3);
+        final Button Jugador4 = (Button) findViewById(R.id.Jugador4);
+
+        Jugador2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Animation anrTl = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate_from_right_to_left);
-                botonJugar.startAnimation(anrTl);
+                Jugador2.startAnimation(anrTl);
                 anrTl.setAnimationListener(new Animation.AnimationListener() {
                     @Override
                     public void onAnimationStart(Animation animation) {
@@ -39,7 +34,6 @@ public class Pantalla_Inicio extends Activity {
                     @Override
                     public void onAnimationEnd(Animation animation) {
                         finish();
-                        startActivity(new Intent(Pantalla_Inicio.this, Seleccionar_Mundo.class));
                     }
 
                     @Override
@@ -49,11 +43,12 @@ public class Pantalla_Inicio extends Activity {
                 });
             }
         });
-        botonAyuda.setOnClickListener(new View.OnClickListener() {
+
+        Jugador3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Animation anlTr = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate_from_left_to_right);
-                botonAyuda.startAnimation(anlTr);
+                Jugador3.startAnimation(anlTr);
                 anlTr.setAnimationListener(new Animation.AnimationListener() {
                     @Override
                     public void onAnimationStart(Animation animation) {
@@ -63,10 +58,6 @@ public class Pantalla_Inicio extends Activity {
                     @Override
                     public void onAnimationEnd(Animation animation) {
                         finish();
-                        String url = "https://docs.google.com/document/d/1DPUn_RA08ynQx5lqpod7afcivpOAeBaWCNKg9Mpmqp0/edit?usp=sharing";
-                        Intent i = new Intent(Intent.ACTION_VIEW);
-                        i.setData(Uri.parse(url));
-                        startActivity(i);
                     }
 
                     @Override
@@ -76,27 +67,30 @@ public class Pantalla_Inicio extends Activity {
                 });
             }
         });
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (fondo.isPlaying()){
-            fondo.stop();
-            fondo.release();
-        }
-    }
+        Jugador4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Animation anlTr = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate_from_left_to_right);
+                Jugador4.startAnimation(anlTr);
+                anlTr.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        fondo.start();
-    }
+                    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        fondo.pause();
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        finish();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/electiva/zimbabue/Pantalla_Tablero.java
+++ app/src/main/java/com/example/electiva/zimbabue/Pantalla_Tablero.java
@@ -10,7 +10,7 @@ public class Pantalla_Tablero extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
     }
 }
 
