d06d786ca1b499f69431c2c8f4b4f31df0704ff7
Ariel Montero Monestel <arielmmonestel@gmail.com>
2016-03-21 14:40:04 -0600

    avance de ventanas

--- app/src/main/java/com/example/electiva/zimbabue/Seleccionar_Mundo.java
+++ app/src/main/java/com/example/electiva/zimbabue/Seleccionar_Mundo.java
@@ -1,8 +1,10 @@
 package com.example.electiva.zimbabue;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Window;
 
 public class Seleccionar_Mundo extends Activity {
@@ -12,5 +14,14 @@ public class Seleccionar_Mundo extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_seleccionar__mundo);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if (keyCode == event.KEYCODE_BACK) {
+            startActivity(new Intent(Seleccionar_Mundo.this, Pantalla_Inicio.class));
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
