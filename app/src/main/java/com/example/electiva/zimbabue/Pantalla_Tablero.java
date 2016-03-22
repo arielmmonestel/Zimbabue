6e63158c4faa9858e2dc9e97cf239c9e6b2b7674
Ariel Montero Monestel <arielmmonestel@gmail.com>
2016-03-22 16:20:51 -0600

    coloque background en las ventanas principal, seleccionar mundo, y cantidad de jugadores

--- app/src/main/java/com/example/electiva/zimbabue/Pantalla_Tablero.java
+++ app/src/main/java/com/example/electiva/zimbabue/Pantalla_Tablero.java
@@ -1,7 +1,9 @@
 package com.example.electiva.zimbabue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class Pantalla_Tablero extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Pantalla_Tablero extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_tablero);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if (keyCode == event.KEYCODE_BACK) {
+            startActivity(new Intent(Pantalla_Tablero.this, Pantalla_Inicio.class));
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
