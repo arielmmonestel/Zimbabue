1addc30a53cc4b952d5becd4864a9b0bc70da26b
GiaccomoU <giaccosup@gmail.com>
2016-04-20 17:40:10 -0600

    Inicio de implementación de la animación y colores

--- app/src/main/java/com/example/electiva/zimbabue/Pantalla_Preguntas.java
+++ app/src/main/java/com/example/electiva/zimbabue/Pantalla_Preguntas.java
@@ -1,12 +1,16 @@
 package com.example.electiva.zimbabue;
 
+import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -27,6 +31,8 @@ public class Pantalla_Preguntas extends AppCompatActivity {
     public static Pregunta preguntaRandom;
     public static ArrayList<Integer> listaRespuestas;
     public static boolean yaEligioPregunta;
+    public static boolean mShowingBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +57,72 @@ public class Pantalla_Preguntas extends AppCompatActivity {
         preguntaRandom = getPreguntaRandom(listaPreguntas);
         listaRespuestas = getRespuestasRandom(listaPreguntas, preguntaRandom);
         yaEligioPregunta = false;
-
+//////////////////////////////////
+        mShowingBack = false;
+        if (savedInstanceState == null) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.relativeLayoutPregA, new CardFrontFragment())
+                    .commit();
+        }
+//////////////////////////////////
     }
+
+
+    private void flipCard() {
+        if (mShowingBack) {
+            getFragmentManager().popBackStack();
+            return;
+        }
+
+        // Flip to the back.
+
+        mShowingBack = true;
+
+        // Create and commit a new fragment transaction that adds the fragment for
+        // the back of the card, uses custom animations, and is part of the fragment
+        // manager's back stack.
+
+        getFragmentManager()
+                .beginTransaction()
+
+                        // Replace the default fragment animations with animator resources
+                        // representing rotations when switching to the back of the card, as
+                        // well as animator resources representing rotations when flipping
+                        // back to the front (e.g. when the system Back button is pressed).
+                .setCustomAnimations(
+                        R.anim.flip_carta_entrada,
+                        R.anim.flip_carta_salida)
+
+                        // Replace any fragments currently in the container view with a
+                        // fragment representing the next page (indicated by the
+                        // just-incremented currentPage variable).
+                .replace(R.id.relativeLayoutPregA, new CardBackFragment())
+
+                        // Add this transaction to the back stack, allowing users to press
+                        // Back to get to the front of the card.
+                .addToBackStack(null)
+
+                        // Commit the transaction.
+                .commit();
+    }
+    /////////////////////////////////////////////////
+    public class CardFrontFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.carta_normal, container, false);
+        }
+    }
+
+    public class CardBackFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.carta_volteada, container, false);
+        }
+    }
+    ////////////////////////////////////////////////
 
     public void seleccionarPreguntaA(View v){
         if(!yaEligioPregunta){
@@ -87,6 +157,31 @@ public class Pantalla_Preguntas extends AppCompatActivity {
             btnOpcionB.setText(String.valueOf(listaRespuestas.get(1)));
             btnOpcionC.setText(String.valueOf(listaRespuestas.get(2)));
             yaEligioPregunta = true;
+        }
+    }
+
+    public void elegirRespuesta(View v){
+        switch(v.getId()){
+            case R.id.buttonOpcionA:
+                if(preguntaRandom.getResultado() == listaRespuestas.get(0)) {
+                    btnOpcionA.setBackground(getDrawable(R.drawable.fondo_opcion_correcta));
+                }else{
+                    btnOpcionA.setBackground(getDrawable(R.drawable.fondo_opcion_incorrecta));
+                }
+                break;
+            case R.id.buttonOpcionB:
+                if(preguntaRandom.getResultado() == listaRespuestas.get(1)) {
+                    btnOpcionB.setBackground(getDrawable(R.drawable.fondo_opcion_correcta));
+                }else{
+                    btnOpcionB.setBackground(getDrawable(R.drawable.fondo_opcion_incorrecta));
+                }
+                break;
+            case R.id.buttonOpcionC:
+                if(preguntaRandom.getResultado() == listaRespuestas.get(2)) {
+                    btnOpcionC.setBackground(getDrawable(R.drawable.fondo_opcion_correcta));
+                }else{
+                    btnOpcionC.setBackground(getDrawable(R.drawable.fondo_opcion_incorrecta));
+                }
         }
     }
 
