b4c903cd1705d595572d4d4901c37b674842cca4
luisrra17 <luisrra17@gmail.com>
2016-04-23 00:55:11 -0600

    Winner

---
+++ app/src/main/java/com/example/electiva/zimbabue/PopWin.java
@@ -0,0 +1,61 @@
+package com.example.electiva.zimbabue;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by LuisA on 20/4/2016.
+ */
+public class PopWin extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        Bundle variables = intent.getExtras();
+        int jugadorEnTurno = variables.getInt("jugadorEnTurno");
+        setContentView(R.layout.popupwindow);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        Typeface texto = Typeface.createFromAsset(getAssets(),"zoo.otf");
+        Typeface textNumero = Typeface.createFromAsset(getAssets(),"number.ttf");
+        TextView myTexto = (TextView) findViewById(R.id.pw1);
+        TextView myTexto2 = (TextView) findViewById(R.id.pw2);
+        myTexto2.setText(String.valueOf(jugadorEnTurno));
+        myTexto2.setTypeface(textNumero);
+        myTexto.setTypeface(texto);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width*.8),(int)(height* 0.6));
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        final CounterClass timer = new CounterClass(4000,1000);
+        timer.start();
+    }
+
+    public class CounterClass extends CountDownTimer {
+        public CounterClass(long millisInfuture, long countDownInterval){
+            super(millisInfuture,countDownInterval);
+        }
+        public  void onTick(long millisUntilFinished){
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+
+
+        }
+        public void onFinish(){
+            finish();
+        }
+
+    }
+}
